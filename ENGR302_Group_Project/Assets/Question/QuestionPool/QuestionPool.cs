da799ebab78c66e9f77e242cb1ca849792a7c57d
jsmall-labb <jamessmall1238@gmail.com>
2025-08-29 09:54:15 +1200

    Starting Question pool code

--- ENGR302_Group_Project/Assets/Question/QuestionPool/QuestionPool.cs
+++ ENGR302_Group_Project/Assets/Question/QuestionPool/QuestionPool.cs
@@ -2,5 +2,18 @@
 
 public class QuestionPool
 {
-    
+    public QuestionPool()
+    {
+        
+    }
+
+    public Question SetupQuestion()
+    {
+        return null;
+    }
+
+    public void LoadQuestions()
+    {
+        
+    }
 }
