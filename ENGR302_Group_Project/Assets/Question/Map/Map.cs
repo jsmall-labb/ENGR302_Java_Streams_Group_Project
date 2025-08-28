af1ec0d66267c01e45ec85ceeca777906eb82381
jsmall-labb <jamessmall1238@gmail.com>
2025-08-29 09:56:14 +1200

    starting map code

--- ENGR302_Group_Project/Assets/Question/Map/Map.cs
+++ ENGR302_Group_Project/Assets/Question/Map/Map.cs
@@ -1,6 +1,23 @@
-﻿namespace DefaultNamespace;
+﻿using System.Collections.Generic;
+
+
 
 public class Map
 {
-    
+    private List<Room> _rooms;
+
+    public Map()
+    {
+        
+    }
+
+    public void LoadRoom()
+    {
+        
+    }
+
+    public Room GetRoom(int index)
+    {
+        return _rooms[index];
+    }
 }
