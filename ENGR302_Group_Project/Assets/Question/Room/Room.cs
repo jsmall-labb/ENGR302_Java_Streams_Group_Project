542d507b99e85b020ab0e7f0b40692f91fa54fae
jsmall-labb <jamessmall1238@gmail.com>
2025-08-29 10:43:42 +1200

    added method to save all answers of question i a room to a local pool of answers for use in game

--- ENGR302_Group_Project/Assets/Question/Room/Room.cs
+++ ENGR302_Group_Project/Assets/Question/Room/Room.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 public class Room
 {
     private int _numTasks;
     private QuestionPool _questionPool;
     private Dictionary<int,Question> _questions;
+    private Dictionary<int, String> _questionAnswers;
     
     public Room(int numtasks, QuestionPool questionPool)
     {
@@ -14,12 +16,23 @@ public class Room
 
     public void Setup()
     {
+        _questions = new Dictionary<int, Question>();
         for (int i = 0; i < _numTasks; i++)
         {
             Question q = _questionPool.GetRandomQuestion();
             _questions.Add(q.getId(), q);
         }
+        GetAllAnswers();
        
+    }
+
+    private void GetAllAnswers()
+    {
+        _questionAnswers = new Dictionary<int, String>();
+        foreach(Question q in _questions.Values)
+        {
+            _questionAnswers.Add(q.getId(), q.GetAnswer());
+        }
     }
 
     public Question GetQuestion(int id)
