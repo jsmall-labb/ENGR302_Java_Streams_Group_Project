a39f7fb6ebc2e0e3ea7becbb14d3d018e0d0ba61
jsmall-labb <jamessmall1238@gmail.com>
2025-08-29 09:52:24 +1200

    Moving Files

---
+++ ENGR302_Group_Project/Assets/Question/Room/Room.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using DefaultNamespace;
+
+
+public class Room
+{
+    private int _numTasks;
+    private QuestionPool _questionPool;
+    private List<Question> _questions;
+    
+    public Room(int numtasks, QuestionPool questionPool)
+    {
+        _numTasks = numtasks;
+        _questionPool = questionPool;
+    }
+
+    public void Setup()
+    {
+        for (int i = 0; i < _numTasks; i++)
+        {
+            Question q = new Question(_questionPool);
+            _questions.Add(q);
+        }
+       
+    }
+
+    public Question GetQuestion(int index)
+    {
+        return _questions[index];
+    }
+
+
+}
