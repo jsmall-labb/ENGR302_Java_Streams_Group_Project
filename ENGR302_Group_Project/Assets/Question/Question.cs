96a5c0b91200c917b162cfee3c7628113d1337ec
jsmall-labb <jamessmall1238@gmail.com>
2025-08-29 09:51:41 +1200

    added field holders and getter methods for question to work

---
+++ ENGR302_Group_Project/Assets/Question/Question.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using DefaultNamespace;
+
+
+public class Question
+{
+
+ private String _room;
+ private String _context;
+ private String _answer;
+ private String _completion;
+ public Question(String  room, String context, String answer, String completion)
+ {
+ _room = room;
+ _context = context;
+ _answer = answer;
+ _completion = completion;
+ }
+
+ public String GetRoom()
+ {
+  return _room;
+ }
+
+ public String GetContext()
+ {
+  return _context;
+ }
+
+ public String GetAnswer()
+ {
+  return _answer;
+ }
+
+ public String GetCompletion()
+ {
+  return _completion;
+ }
+
+ public bool IsCompleted(String input)
+ {
+  return input == _answer;
+ }
+}
