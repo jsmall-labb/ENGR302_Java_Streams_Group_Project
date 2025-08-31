b73ebf8fd3a7a34b6e20ad5ad15c84245bfd3324
Ziggy Redding <ziggy.deely@gmail.com>
2025-09-01 02:06:38 +1200

    Added json containing the questions, and a parser that returns the questions.

--- ENGR302_Group_Project/Assets/Question/Json/JsonReader/JsonReader.cs
+++ ENGR302_Group_Project/Assets/Question/Json/JsonReader/JsonReader.cs
@@ -1,5 +1,56 @@
 ﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+// class QuestionList
+// {
+//     public List<Question> questions;
+// }
+
+[System.Serializable]
+public class JSONQuestion
+{
+    public string room;
+    public string text;
+    public List<string> answer;
+    public string completed_text;
+}
+
+[System.Serializable]
+public class JSONLoadedQuestions
+{
+    public List<JSONQuestion> questions;
+}
+
 public class JsonReader
 {
-    
+    private static TextAsset json = Resources.Load<TextAsset>("questions");
+    private static int currentID = 0;
+    private static List<Question> returnQuestions = new();
+
+    public static List<Question> GetAllQuestions()
+    {
+        if (returnQuestions.Count != 0) { return returnQuestions; }
+        if (json == null) { Debug.Log("JSON file could not be loaded."); }
+
+        JSONLoadedQuestions jsonLoadedQuestions = JsonUtility.FromJson<JSONLoadedQuestions>(json.text);
+
+        foreach (JSONQuestion jq in jsonLoadedQuestions.questions)
+        {
+            // Temporary solution until a different method is chosen.
+            string answerText = "";
+            foreach (string s in jq.answer)
+            {
+                answerText += s;
+            }
+
+            returnQuestions.Add(new Question(currentID, jq.room, jq.text, answerText, jq.completed_text));
+            currentID++;
+        }
+
+        return returnQuestions;
+
+    }
 }
