86a9510b37126a5bd4819c386bf1afb1e4ca3f2d
Ziggy Redding <ziggy.deely@gmail.com>
2025-09-11 14:54:46 +1200

    Added completion check

--- ENGR302_Group_Project/Assets/Question/TaskScreen/TaskScreen.cs
+++ ENGR302_Group_Project/Assets/Question/TaskScreen/TaskScreen.cs
@@ -11,25 +11,42 @@ public class TaskScreen : MonoBehaviour
     public Transform buttonPanel;    // Parent object to hold the buttons (e.g. a UI Panel)
     public int numberOfButtons = 5;  // Number of buttons you want to generate
     private List<Button> buttons = new List<Button>();   // List to hold buttons
-    private List<string> clickOrder = new List<string>(); // List to track button click order
-    private int qindex = 7;
+
+    private List<string> answers = new();
+
     private Text text;
+    private Question q;
+
+
+
+    // Needs to be removed for when called by prefab.
     void Start()
     {
         text = GetComponent<Text>();
 
         // Generate buttons based on the specified number
-        Question q = JsonReader.GetAllQuestions()[qindex];
+        // Question q = JsonReader.GetAllQuestions()[qindex];
+        JsonReader jr = new();
+        q = jr.GetAllQuestions()[7];
+
         text.text = q.GetContext();
         CreateButtons(q.GetAnswer().Count);
     }
 
+
+    // Prefab opens this one.
+    void Execute(Question _q)
+    {
+
+        text = GetComponent<Text>();
+        q = _q;
+        text.text = q.GetContext();
+        CreateButtons(q.GetAnswer().Count);
+    }
+
+
     void CreateButtons(int buttonCount)
-    {   
-
-
-
-    
+    {
         // Clear any existing buttons
         foreach (Transform child in buttonPanel)
         {
@@ -42,25 +59,43 @@ public class TaskScreen : MonoBehaviour
             // Create a new button from the prefab
             Button newButton = Instantiate(buttonPrefab, buttonPanel).GetComponent<Button>();
             // newButton.GetComponentInChildren<Text>().text = "Button " + (i + 1); // Set button text
-            newButton.GetComponentInChildren<Text>().text = JsonReader.GetAllQuestions()[qindex].GetAnswer()[i]; // Set button text
+            newButton.GetComponentInChildren<Text>().text = q.GetAnswer()[i]; // Set button text
             Text tc = newButton.GetComponentInChildren<Text>();
-            tc.fontSize = 20;   
-            // newButton.transform.position = new Vector3(Screen.width / (i+1), Screen.height / 2, 0);
+            tc.fontSize = 20;
 
             int index = i;  // Capture the index to avoid closure issues
             newButton.onClick.AddListener(() => ButtonClicked(index, newButton));  // Add listener for button click
             buttons.Add(newButton);
         }
+
+        Button complete_button = Instantiate(buttonPrefab, buttonPanel).GetComponent<Button>();
+        Text button_text = complete_button.GetComponentInChildren<Text>();
+        button_text.text = "Submit";
+        button_text.fontSize = 20;
+
+        complete_button.onClick.AddListener(() =>
+        {
+            checkComplete();
+        });
+
+    }
+
+    void checkComplete()
+    {
+        if (answers.Count == q.GetAnswer().Count && q.IsCorrect(answers))
+        {
+            Debug.Log("Correct");
+        }
+        else
+        {
+            Debug.Log("Incorrect");
+        }
     }
 
     void ButtonClicked(int index, Button b)
     {
-        // Track the order of clicks
-        // clickOrder.Add("Button " + (index + 1));
 
-        // Update the UI text to show the current order of clicks
-        // orderText.text = "Click Order: " + string.Join(", ", clickOrder);
-        string text_ = JsonReader.GetAllQuestions()[qindex].GetAnswer()[index];
+        string text_ = q.GetAnswer()[index];
         text_ = text_.Substring(0, text_.Length - 2);
 
         Debug.Log("Button : " + text_);
@@ -80,6 +115,7 @@ public class TaskScreen : MonoBehaviour
             Color c = i.color;
             c.a = 1f;
             i.color = c;
+            answers.Remove(q.GetAnswer()[index]);
         }
         else
         {
@@ -97,10 +133,8 @@ public class TaskScreen : MonoBehaviour
             Color c = i.color;
             c.a = 0.25f;
             i.color = c;
+            answers.Add(q.GetAnswer()[index]);
         }
-
-
-
 
     }
 }
